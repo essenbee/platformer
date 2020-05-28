7971108ab0b522cb7df7b294933ec6438803ceca
Stuart Bonham <stuart_bonham@blueyonder.co.uk>
2020-05-28 17:55:56 +0100

    Haxe 4.1.1

--- export/html5/haxe/ApplicationMain.hx
+++ export/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "12";
+		app.meta["build"] = "13";
 		app.meta["company"] = "CodebaseAlpha";
 		app.meta["file"] = "platformer";
 		app.meta["name"] = "platformer";
